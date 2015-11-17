cf7e9dbe23e239ad66cd53f45b0e475535f76ba8
Kristof Torfs <kristof.torfs@econoweb.be>
2015-11-17 16:47:12 +0100

    Initial commit

---
+++ materialize.d.ts
@@ -0,0 +1,21 @@
+﻿// Type definitions for Bootstrap 3.3.5
+// Project: http://materializecss.com/
+// Definitions by: Kristof Torfs <https://github.com/kristoftorfs/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+
+/// <reference path="../jquery/jquery.d.ts"/>
+
+interface SideNavOptions {
+    menuWidth?: number;
+    edge?: string;
+    closeOnClick?: boolean;
+}
+
+interface JQuery {
+    sideNav(options?: SideNavOptions): JQuery;
+    SideNav(command: string): JQuery;
+}
+
+declare module "materialize" {
+}
